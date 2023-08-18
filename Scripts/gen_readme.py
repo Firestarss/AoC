548b284b29e3e21b43d1a0ee5da384e74393734d
Florian Schwarzinger <firestarss88@gmail.com>
2023-08-18 16:50:48 -0400

    update readme

--- Scripts/gen_readme.py
+++ Scripts/gen_readme.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
         md_txt += f"\n| [{line[0]}]({line[0]}) | {line[1].zfill(2):5} | {progressBar(int(line[1]), 50, 50, '*', ' ')} | {progress_percent(int(line[1]),50)} |"
 
-    md_txt += f"\n| {'Total'.ljust(12)} | {str(total_sum).ljust(5)} | {progressBar(total_sum, 50 * len(stars), 50, 'l', ' ')} | {progress_percent(total_sum, 50 * len(stars))} |"
+    md_txt += f"\n| {'Total'.ljust(12)} | {str(total_sum).ljust(5)} | {progressBar(total_sum, 50 * len(stars), 50, '*', ' ')} | {progress_percent(total_sum, 50 * len(stars))} |"
     md_txt += "\n"
 
     print(md_txt)
