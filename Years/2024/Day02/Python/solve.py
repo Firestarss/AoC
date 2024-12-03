24330afe3a47fcc4fbfb611403308d5f5fb257b3
Florian Schwarzinger <firestarss88@gmail.com>
2024-12-02 23:24:16 -0500

    Qol updates

--- Years/2024/Day02/Python/solve.py
+++ Years/2024/Day02/Python/solve.py
@@ -5,16 +5,12 @@ with open(input_files[file], 'r') as f:
     lines =  [list(map(int,a.strip().split())) for a in f.readlines()]
 
 def valid_level(level):
+    if level not in [sorted(level), sorted(level)[::-1]]:
+        return False
     for i in range(len(level) -1):
             dist = abs(level[i] - level[i+1])
             if dist < 1 or dist > 3:
                 return False
-            if level[0] > level[1]:
-                if level[i] < level[i+1]:
-                    return False
-            if level[0] < level[1]:
-                if level[i] > level[i+1]:
-                    return False
                 
     return True
 
@@ -27,31 +23,10 @@ def all_levels(level):
     return False
 
 def part1():
-    count = len(lines)
-    for level in lines:
-        for i in range(len(level) -1):
-            dist = abs(level[i] - level[i+1])
-            if dist < 1 or dist > 3:
-                count -= 1
-                break
-            if level[0] > level[1]:
-                if level[i] < level[i+1]:
-                    count -= 1
-                    break
-            if level[0] < level[1]:
-                if level[i] > level[i+1]:
-                    count -= 1
-                    break
-
-    print(count)
+    print(sum([valid_level(level) for level in lines]))
 
 def part2():
-    count = len(lines)
-    for level in lines:
-        if not all_levels(level):
-            count -=1
-    
-    print(count)
+    print(sum([all_levels(level) for level in lines]))
 
 
 part1()
